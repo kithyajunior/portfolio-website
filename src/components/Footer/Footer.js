f68cccdce70cfed57f8bd2152c379536fe110543
Kosal Pen <kosalpen99@gmail.com>
2025-07-16 14:47:04 +0700

    update infomation

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ export const Footer = ({ className }) => (
   <footer className={classes(styles.footer, className)}>
     <Text size="s" align="center">
       <span className={styles.date}>
-        {`© ${new Date().getFullYear()} Kosal Pen.`}
+        {`© ${new Date().getFullYear()} Hamish Williams.`}
       </span>
       <Link secondary className={styles.link} href="/humans.txt" target="_self">
         Crafted by yours truly
